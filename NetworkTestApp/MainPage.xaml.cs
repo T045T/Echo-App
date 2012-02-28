5630527814cb7732f8ca7a9f237c7a4bc225b249
Moritz Winter <moritz.winter@gmail.com>
2012-02-28 14:19:45 +0100

    added debug code to NetworkTestApp

--- NetworkTestApp/MainPage.xaml.cs
+++ NetworkTestApp/MainPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using TestApp.echo;
 
 namespace NetworkTestApp
 {
@@ -19,6 +20,11 @@ namespace NetworkTestApp
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void PageTitle_Tap(object sender, GestureEventArgs e)
+        {
+            var testcon = new Connection();
         }
     }
 }
