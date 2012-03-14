6a4827af667d7a1f4e961489d1ff612579491ade
Nils Berg <berg.nils@gmail.com>
2012-03-14 14:22:42 +0100

    reduced pointless whitespace

--- Echo/Helpers/DateTimeConverters.cs
+++ Echo/Helpers/DateTimeConverters.cs
@@ -1,13 +1,4 @@
 ﻿using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using System.Windows.Data;
 using System.Globalization;
 
@@ -17,42 +8,28 @@ namespace Echo.Helpers
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-
             var dateTime = (DateTime)value;
-
             return dateTime.ToShortTimeString();
         }
 
-
-
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-
             throw new NotSupportedException();
-
         }
-
     }
 
     public class ShortDateConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-
             var dateTime = (DateTime)value;
-
             return dateTime.ToShortDateString();
         }
 
-
-
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-
             throw new NotSupportedException();
-
         }
-
     }
 
 }
