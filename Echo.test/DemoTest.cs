1ed4aad30816cef84ee80ac299081e2b5fd6ad25
Nils Berg <berg.nils@gmail.com>
2012-03-14 14:23:54 +0100

    started test project and downloaded unit testing assemblies

---
+++ Echo.test/DemoTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class DemoTest
+{
+    [TestMethod]
+    public void MyFirstUnitTest()
+    {
+        Assert.IsFalse(false);
+        //Assert.Inconclusive("We need to write our first unit test");
+    }
+}
---
+++ Echo.test/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Phone.Controls;
+using Microsoft.Silverlight.Testing;
+
+namespace Echo.test
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Konstruktor
+        public MainPage()
+        {
+            InitializeComponent();
+            Content = UnitTestSystem.CreateTestPage();
+        }
+    }
+}
